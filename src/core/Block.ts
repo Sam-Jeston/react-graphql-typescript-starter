6e1e0ed92a7834d9b84dfafc6a0c97f2750c40b6
Sam Jeston <sam.jeston@iohk.io>
2019-08-21 16:30:20 +1000

    Commenting regarding origin of core types

--- src/core/Block.ts
+++ src/core/Block.ts
@@ -1,5 +1,6 @@
 import { Transaction } from './Transaction'
 
+// This will be imported from a package, just adding for demo
 export interface Block {
   id: number
   transactions: Transaction[]
--- src/core/Transaction.ts
+++ src/core/Transaction.ts
@@ -1,3 +1,4 @@
+// This will be imported from a package, just adding for demo
 export interface Transaction {
   hash: string
 }
